5b1c2dfba67d65dd3f4b3475ce27cefc26474fa4
Thamuz <nguyenhoanggiaminh24@gmail.com>
2024-03-20 08:43:18 +0700

    first commit

---
+++ src/main/java/com/unkanpo/repository/TypeRepository.java
@@ -0,0 +1,7 @@
+package com.unkanpo.repository;
+
+import com.unkanpo.model.Type;
+import org.springframework.data.repository.CrudRepository;
+
+public interface TypeRepository extends CrudRepository<Type,Long> {
+}
---
+++ src/main/java/com/unkanpo/service/IRoleService.java
@@ -0,0 +1,11 @@
+//package com.unkanpo.service;
+//
+//import com.unkanpo.model.Role;
+//
+//public interface IRoleService {
+//    Iterable<Role> findAll();
+//
+//    void save(Role role);
+//
+//    Role findByName(String name);
+//}
