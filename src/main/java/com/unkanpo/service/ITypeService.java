6cd7d69d9e0f6906cf81a69e2b4a03251310a0dd
LouisFores <146702670+LouisFores@users.noreply.github.com>
2024-03-24 16:36:44 +0700

    Update ITypeService.java

--- src/main/java/com/unkanpo/service/ITypeService.java
+++ src/main/java/com/unkanpo/service/ITypeService.java
@@ -7,10 +7,8 @@ import java.util.Optional;
 public interface ITypeService {
 
     Iterable<Type> findAll();
-    Iterable<Type> findAllByName_type(String key_word);
+    Iterable<Type> findAllByNameType(String keyword);
     Optional<Type> findById(Long id);
     void save(Type type);
-    void deleteById(Long id);
-
 
 }
