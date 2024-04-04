610fb21d6f0259a207bd2f861286526ff74e00b3
thamuz24 <nguyenhoanggiaminh24@gmail.com>
2024-04-04 22:57:06 +0700

    update

--- src/main/java/com/unkanpo/controller/GameController.java
+++ src/main/java/com/unkanpo/controller/GameController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.*;
@@ -44,8 +45,8 @@ public class GameController {
 
     @PostMapping("/create")
     @Transactional
-    public String createGame(@ModelAttribute("gameForm") GameForm game) {
-        gameService.save(game);
+    public String createGame(@ModelAttribute("gameForm") GameForm game, @RequestParam("more-image") List<MultipartFile> image) {
+        gameService.save(game,image);
         return "redirect:/admin/games";
     }
 
